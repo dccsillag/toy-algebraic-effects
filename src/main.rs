4b1fdddefe3bce7441f3789a4167d461f5845249
Daniel Csillag <dccsillag@gmail.com>
2023-04-13 18:26:24 -0300

    Add built-in functions

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ enum AST {
 
 #[derive(Clone)]
 enum Value {
+    BuiltinFunction(Rc<dyn Fn(Value) -> Result<Value, Error>>),
     Function(Rc<RefCell<Context>>, Variable, Box<AST>),
     Bool(bool),
     Int(i64),
@@ -62,17 +63,18 @@ fn interpret(ast: &AST, context: &mut Context) -> Result<Value, Error> {
             *bound_var,
             body.clone(),
         )),
-        AST::Application(f, x) => match interpret(f, context)? {
-            Value::Function(closure_context, bound_var, body) => {
-                let arg = interpret(x, context)?;
-                closure_context
+        AST::Application(f, x) => {
+            let arg = interpret(x, context)?;
+            match interpret(f, context)? {
+                Value::BuiltinFunction(func) => func(arg),
+                Value::Function(closure_context, bound_var, body) => closure_context
                     .borrow_mut()
-                    .with_var(bound_var, arg, |context| interpret(&body, context))
+                    .with_var(bound_var, arg, |context| interpret(&body, context)),
+                val @ (Value::Bool(_) | Value::Int(_) | Value::String(_)) => {
+                    Err(Error::NotACallableValue(val))
+                }
             }
-            val @ (Value::Bool(_) | Value::Int(_) | Value::String(_)) => {
-                Err(Error::NotACallableValue(val))
-            }
-        },
+        }
         AST::Variable(var) => match context.lookup(var) {
             Some(out) => Ok(out.clone()),
             None => Err(Error::NotInScope(*var)),
@@ -80,9 +82,10 @@ fn interpret(ast: &AST, context: &mut Context) -> Result<Value, Error> {
         AST::Const(val) => Ok(val.clone()),
         AST::Cond(cond, then, r#else) => match interpret(cond, context)? {
             Value::Bool(b) => interpret(if b { then } else { r#else }, context),
-            val @ (Value::Function(_, _, _) | Value::Int(_) | Value::String(_)) => {
-                Err(Error::NotABoolValue(val))
-            }
+            val @ (Value::BuiltinFunction(_)
+            | Value::Function(_, _, _)
+            | Value::Int(_)
+            | Value::String(_)) => Err(Error::NotABoolValue(val)),
         },
     }
 }
